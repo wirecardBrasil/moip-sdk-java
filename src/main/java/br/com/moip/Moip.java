0a757ea5d0c6f9e3013feb31e130e24d7e245326
mbnakaya <mbnakaya25@gmail.com>
2018-06-04 17:19:07 -0300

    adds basic settings and basic auth

---
+++ src/main/java/br/com/moip/Moip.java
@@ -0,0 +1,38 @@
+package br.com.moip;
+
+public class Moip {
+
+    private static final String SANDBOX_URL = "https://sandbox.moip.com.br/v2";
+
+    private static final String PRODUCTION_URL = "https://api.moip.com.br/v2";
+
+    private static final String CONNECT_SANDBOX_URL = "https://connect-sandbox.moip.com.br";
+
+    private static final String CONNECT_PRODUCTION_URL = "https://connect.moip.com.br";
+
+    // Default connect timeout (in milliseconds) with default value.
+    private static int CONNECT_TIMEOUT = 60 * 1000;
+
+    // Read timeout (in milliseconds) with default value.
+    private static int READ_TIMEOUT = 80 * 1000;
+
+    public static int getConnectTimeout() { return CONNECT_TIMEOUT; }
+
+    public static int getReadTimeout() { return READ_TIMEOUT; }
+
+    /**
+     * Use this method will change the connect timeout default value.
+     * It will be used to request the Moip APIs.
+     *
+     * @param connectTimeout integer timeout in milliseconds
+     */
+    public static void setConnectTimeout(final int connectTimeout) { CONNECT_TIMEOUT = connectTimeout; }
+
+    /**
+     * Use this method will change the read timeout default value.
+     * It will be used to request the Moip APIs.
+     *
+     * @param readTimeout integer timeout in millisecond
+     */
+    public static void setReadTimeout(final int readTimeout) { READ_TIMEOUT = readTimeout; }
+}
---
+++ src/main/java/br/com/moip/auth/BasicAuth.java
@@ -0,0 +1,25 @@
+package br.com.moip.auth;
+
+import java.net.HttpURLConnection;
+
+public class BasicAuth implements Authentication {
+
+    private final String token;
+    private final String key;
+
+    public BasicAuth(String token, String key) {
+        this.token = token;
+        this.key = key;
+    }
+
+    public String getToken() { return token; }
+
+    public String getKey() { return key; }
+
+    @Override
+    public void authenticate(HttpURLConnection connection) {
+        String authorization = Base64.encode((token + ":" + key).getBytes());
+
+        connection.setRequestProperty("Authorization", "Basic " + authorization);
+    }
+}

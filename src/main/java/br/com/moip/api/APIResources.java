cae718a6cfce45516ba6ce862cc6516ea8904866
mbnakaya <mbnakaya25@gmail.com>
2018-08-01 14:25:57 -0300

    APIResources: fix imports

--- src/main/java/br/com/moip/api/APIResources.java
+++ src/main/java/br/com/moip/api/APIResources.java
@@ -1,6 +1,12 @@
 package br.com.moip.api;
-
-import br.com.moip.models.*;
+;
+import br.com.moip.models.Customer;
+import br.com.moip.models.Order;
+import br.com.moip.models.Payment;
+import br.com.moip.models.Refund;
+import br.com.moip.models.NotificationPreference;
+import br.com.moip.models.Webhook;
+import br.com.moip.models.Account;
 
 public class APIResources {
 
